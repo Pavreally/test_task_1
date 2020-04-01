60848b8502d55661f12e0c429c2dfc6d0c99cfea
TIALLLA <pavreally@gmail.com>
2020-04-01 12:00:42 +0400

    Test Task. Part 3. Patch 1: optimization.

--- test_task/js/app.js
+++ test_task/js/app.js
@@ -1,58 +1,62 @@
 $(document).foundation();
 
+var itemTitle = 'h4',
+    itemTitleChangeInput = 'input',
+    itemTitleChangeButton = 'button',
+    itemPrice = 'h5',
+    itemDescription = '.description-item';
+
 // Price list trigger styles
 $('.trigger-view-styles').mouseover(function () {
 
   if ($('#form-items').hasClass('block')) {
-
     $('#form-items').removeClass('block');
     $('#form-items').addClass('table');
 
     $('.table li').removeClass('large-3 medium-4 small-12');
     $('.table li').addClass('large-12');
 
-    $('.item').addClass('grid-x align-justify');
+    $('.inner-items').addClass('grid-x align-justify');
 
-    $('.item img').addClass('cell large-1 medium-12 small-12');
-    $('.item h4').addClass('cell large-2 medium-12 small-12');
-    $('.item .description-item').addClass('cell large-5 medium-12 small-12');
-    $('.item button').addClass('cell large-2 medium-12 small-12');
-    $('.item h5').addClass('cell large-2 medium-12 small-12');
+    $('.inner-items img').addClass('cell large-1 medium-12 small-12');
+    $('.inner-items ' + itemTitle).addClass('cell large-2 medium-12 small-12');
+    $('.inner-items ' + itemDescription).addClass('cell large-5 medium-12 small-12');
+    $('.inner-items ' + itemTitleChangeButton).addClass('cell large-2 medium-12 small-12');
+    $('.inner-items ' + itemPrice).addClass('cell large-2 medium-12 small-12');
   } else {
-
     $('#form-items').removeClass('table');
     $('#form-items').addClass('block');
 
     $('.block li').removeClass('large-12');
     $('.block li').addClass('large-3 medium-4 small-12');
 
-    $('.item').removeClass('grid-x align-justify');
+    $('.inner-items').removeClass('grid-x align-justify');
 
-    $('.item img').removeClass('cell large-1 medium-12 small-12');
-    $('.item h4').removeClass('cell large-2 medium-12 small-12');
-    $('.item .description-item').removeClass('cell large-5 medium-12 small-12');
-    $('.item button').removeClass('cell large-2 medium-12 small-12');
-    $('.item h5').removeClass('cell large-2 medium-12 small-12');
+    $('.inner-items img').removeClass('cell large-1 medium-12 small-12');
+    $('.inner-items ' + itemTitle).removeClass('cell large-2 medium-12 small-12');
+    $('.inner-items ' + itemDescription).removeClass('cell large-5 medium-12 small-12');
+    $('.inner-items ' + itemTitleChangeButton).removeClass('cell large-2 medium-12 small-12');
+    $('.inner-items ' + itemPrice).removeClass('cell large-2 medium-12 small-12');
   }
-
 });
 
 $(document).ready(function () {
 
-  $('.block-items .item').each(function(i) {
-    var targetItem = i + 1;
+  // Title item change
+  $('.block-items .inner-items').each(function(i) {
+    i = i + 1;
 
-    if ($('#edit-name-item-' + targetItem).hasClass('hide')) {
-      $('#name-item-' + targetItem).click(function () {
-        $('#edit-name-item-' + targetItem).removeClass('hide');
-      });
-    }
+    $('#item-' + i + ' ' + itemTitle).click(function () {
+      if ($('#item-' + i + ' #form-title-item').hasClass('hide')) {
+        $('#item-' + i + ' #form-title-item').removeClass('hide');
+      }
+    });
 
-    $('#edit-name-item-' + targetItem + ' input').val($('#name-item-' + targetItem).text());
+    $('#item-' + i + ' #form-title-item ' + itemTitleChangeInput).val($('#item-' + i + ' ' + itemTitle).text());
 
-    $('#edit-name-item-' + targetItem + ' button').click(function () {
-      $('#name-item-' + targetItem).text($('#edit-name-item-' + targetItem + ' input').val());
-      $('#edit-name-item-' + targetItem).addClass('hide');
+    $('#item-' + i + ' #form-title-item ' + itemTitleChangeButton).click(function () {
+      $('#item-' + i + ' ' + itemTitle).text($('#item-' + i + ' #form-title-item ' + itemTitleChangeInput).val());
+      $('#item-' + i + ' #form-title-item').addClass('hide');
       return false; // disable form submission and to page reload
     });
   });
@@ -62,5 +66,4 @@ $(document).ready(function () {
     $('#form-items').sortable();
     $('#form-items').disableSelection();
   });
-
 });
